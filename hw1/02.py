fe7865b46288dc8191aa2841762fed3603ab72e3
joemooretv <13joesjokes@mail.ru>
2020-08-07 08:30:40 +0300

    1-4

--- hw1/02.py
+++ hw1/02.py
@@ -1,10 +1,5 @@
-min = 00
-hour = 00
 time = int(input('Введи время в секундах: '))
 sec = time % 60
-if 1 <= time / 60:
-    min = time // 60
-    if min > 59:
-        min = min % 60
-        hour = min // 60
-print("{}:{}:{}".format(hour, min, sec))
+min = time // 60
+hour = min // 60
+print("{:02d}:{:02d}:{:02d}".format(hour, min % 60, sec))
---
+++ hw1/03.py
@@ -0,0 +1,3 @@
+n = input('Введи положительное число: ')
+sum = int(n) + int(n + n) + int(n + n + n)
+print('Сумма в формате n + nn + nnn равна', sum)

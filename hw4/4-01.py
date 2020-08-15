084225606259f965a237fc2b9c1039d44a11842c
13joesjokes@mail.ru <g3t11hAp>
2020-08-15 15:01:23 +0300

    + 4-1

---
+++ hw4/4-01.py
@@ -0,0 +1,13 @@
+from sys import argv
+
+
+def salary(hours, price, extra):
+    return int(hours) * int(price) + int(extra)
+
+try:
+    script, a, b, c = argv
+    print(salary(int(a), int(b), int(c)))
+except ValueError:
+    print('wrong argument format')
+
+print((22 * 33) + 7)

c2712cf0ec19d6e99a871d8f3b1fec62c67ce5a9
13joesjokes@mail.ru <g3t11hAp>
2020-08-23 17:11:59 +0300

    + 5-1

---
+++ hw5/5-01.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+# Создаем пустой список и наполняем
+# пока пользователь не введет пустую строку
+content = []
+while True:
+    line = input('Enter text line to save:\n')
+    if not line:
+        break
+    content.append(line + '\n')
+
+# Получаем текущие дату и время
+dt = datetime.now().strftime("_%d.%m.%Y_%H:%M:%S")
+
+# Сохраняем в файл
+with open(f'list{dt}.txt', 'w') as file:
+    file.writelines(content)
+    print(f'Your data saved in \"list{dt}.txt\" file')
